6583ffb87ab762e80f1d7863e0d0dcec77647448
Anabln8 <anabln26@gmail.com>
2025-04-09 20:17:45 +0200

    v1.0.0

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
       <div className="relative z-10 px-6 md:px-12">
         <h1 className="text-4xl md:text-6xl font-bold mb-4">Bienvenido a Mi Tienda</h1>
         <p className="text-lg md:text-xl mb-6">Encuentra los mejores productos al mejor precio</p>
-        <a href="#productos" className="bg-blue-500 px-6 py-3 text-lg font-semibold rounded-lg hover:bg-blue-600 transition duration-300">Ver Productos</a>
+        <a href="#productos" className="bg-[#af6b6b] px-6 py-3 text-lg font-semibold rounded-lg hover:bg-[#6a3f3f] transition duration-300">Ver Productos</a>
       </div>
     </section>
   );
